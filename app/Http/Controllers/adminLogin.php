9c50e5fc10c946b06f82e7eb2c244972e214b630
scrip20 <josemontes27000@gmail.com>
2024-04-20 21:42:13 -0500

    Initial commit

---
+++ app/Http/Controllers/adminLogin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    // ...
+
+    public function adminLogin(Request $request)
+    {
+        // Código para el inicio de sesión del administrador
+    }
+
+    // ...
+}
